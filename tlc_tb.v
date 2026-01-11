3f16a7086bc928d2440a9b674b5d4b61511995f0
vardhanpolu61 <123734180+vardhanpolu61@users.noreply.github.com>
2026-01-11 15:59:57 +0530

    Update tlc_tb.v

--- tlc_tb.v
+++ tlc_tb.v
@@ -1,21 +1,49 @@
+// Code your testbench here
 `timescale 1ns / 1ps
+
 module tlc_tb;
-reg x,clr,clk;
-wire [1:0]hwy,ctrd;
-tlc dut(x,clr,hwy,ctrd,clk);
-initial begin
-clk=0;
-forever #5 clk=~clk;
-end
-initial
-begin
-#0 clr=1;
-#2 clr=0;
-#3 x=0;
-#10 x=1;
-#50 x=0;
-#100 x=1;
-#20 x=0;
-#1000 $finish;
-end
+
+  reg x, clr, clk;
+  wire [1:0] hwy, ctrd;
+
+  // DUT instantiation
+  tlc dut (x, clr, hwy, ctrd, clk);
+
+  // Clock generation
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+
+  // Stimulus
+  initial begin
+    clr = 1;
+    x   = 0;
+
+    #2  clr = 0;
+    #3  x   = 0;
+    #10 x   = 1;
+    #70 x   = 0;
+    #100 x  = 1;
+    #100 x   = 0;
+
+    #1000 $finish;
+  end
+
+  // Waveform dump
+  initial begin
+    $dumpfile("tlc_tb.vcd");
+    $dumpvars(0, tlc_tb);
+  end
+
+  // Monitor present state and next state
+  initial begin
+    $monitor(
+      "Time=%0t | clk=%b clr=%b x=%b | PS=%b NS=%b | hwy=%b ctrd=%b",
+      $time, clk, clr, x,
+      dut.state, dut.next_state,
+      hwy, ctrd
+    );
+  end
+
 endmodule
